2d608b6dd63577478e4f44997b70c9f14032a25d
Kolodziej77 <Damian.kolodziejczyk17@gmail.com>
2025-05-13 21:45:58 +0200

    Initializing web app

---
+++ WebApp/environmental/src/main/java/com/environmental/EnvironmentMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.environmental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvironmentMonitoringApplication.class, args);
+	}
+
+}
---
+++ WebApp/environmental/src/test/java/com/environmental/EnvironmentMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.environmental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
